f39ac246c5c3f2a3920159339675690a527505d4
raine <raine.correa@wicomm.com.br>
2026-02-10 11:42:39 -0300

    feat(config): configuracoes test

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@/generated/prisma/client';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const prismaClient = new PrismaClient(undefined as any);
 
 export { prismaClient as prisma };
