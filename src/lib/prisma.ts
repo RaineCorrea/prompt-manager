58815e2dbb6bf7c906f849049a6c43311f4eb0a1
raine <raine.correa@wicomm.com.br>
2026-03-02 15:59:45 -0300

    fix(prisma): algumas correcoes prisma e criacao seed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  throw new Error('DATABASE_URL environment variable is not defined');
-}
+const pool = new Pool({ connectionString });
 
-const adapter = new PrismaPg({
-  connectionString,
-});
+const adapter = new PrismaPg(pool);
 
-const prismaClient = new PrismaClient({ adapter });
-
-export { prismaClient as prisma };
+export const prisma = new PrismaClient({ adapter });
