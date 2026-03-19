df80074868cb1a91677902bf0fc5d9781964adf4
raine <raine.correa@wicomm.com.br>
2026-03-19 10:57:30 -0300

    feat(page): criado nova page para o formulario, utilizacao do react-hook-form, zod e configuracao do form

---
+++ src/app/new/page.tsx
@@ -0,0 +1,5 @@
+import { PromprtForm } from '@/components/prompts';
+
+export default function PromptNew() {
+  return <PromprtForm />;
+}
---
+++ src/components/prompts/prompt-form.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+import {
+  CreatePromptDTO,
+  createPromptSchema,
+} from '@/core/application/prompts/create-prompt.dto';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '../ui/form';
+
+export const PromprtForm = () => {
+  const form = useForm<CreatePromptDTO>({
+    resolver: zodResolver(createPromptSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+    },
+  });
+  return (
+    <Form {...form}>
+      <form action="" className="space-y-6">
+        <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
+          <Button type="submit" size="sm">
+            Salvar
+          </Button>
+        </header>
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder="Título do prompt"
+                  variant="transparent"
+                  size="lg"
+                  autoFocus
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea
+                  placeholder="Digite o conteúdo do prompt..."
+                  variant="transparent"
+                  size="lg"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
